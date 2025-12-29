90825bd0c2bc2a5635b807729ff320700669f3ff
giulio-alfieri <giulio.alfieri@gmail.com>
2025-12-29 11:59:48 +0000

    feat(database): update database schema and import scripts for vertical and horizontal complexes

--- scripts/db_creation.sql
+++ scripts/db_creation.sql
@@ -86,93 +86,6 @@ DEFAULT CHARACTER SET = utf8mb3;
 
 
 -- -----------------------------------------------------
--- Table `toq_db`.`complex`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `toq_db`.`complex` ;
-
-CREATE TABLE IF NOT EXISTS `toq_db`.`complex` (
-  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(255) NOT NULL,
-  `zip_code` VARCHAR(9) NOT NULL,
-  `street` VARCHAR(255) NULL DEFAULT NULL,
-  `number` VARCHAR(15) NOT NULL,
-  `neighborhood` VARCHAR(150) NULL,
-  `city` VARCHAR(150) NOT NULL,
-  `state` VARCHAR(2) NOT NULL,
-  `reception_phone` VARCHAR(25) NULL DEFAULT NULL,
-  `sector` TINYINT UNSIGNED NOT NULL,
-  `main_registration` VARCHAR(45) NULL DEFAULT NULL,
-  `type` INT NOT NULL,
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb3;
-
-
--- -----------------------------------------------------
--- Table `toq_db`.`complex_sizes`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `toq_db`.`complex_sizes` ;
-
-CREATE TABLE IF NOT EXISTS `toq_db`.`complex_sizes` (
-  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  `complex_id` INT UNSIGNED NOT NULL,
-  `size` FLOAT UNSIGNED NOT NULL,
-  `description` VARCHAR(255) NULL,
-  PRIMARY KEY (`id`),
-  INDEX `fk_complex_sizes_idx` (`complex_id` ASC) VISIBLE,
-  CONSTRAINT `fk_complex_sizes`
-    FOREIGN KEY (`complex_id`)
-    REFERENCES `toq_db`.`complex` (`id`)
-    ON DELETE CASCADE)
-ENGINE = InnoDB
-AUTO_INCREMENT = 546
-DEFAULT CHARACTER SET = utf8mb3;
-
-
--- -----------------------------------------------------
--- Table `toq_db`.`complex_towers`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `toq_db`.`complex_towers` ;
-
-CREATE TABLE IF NOT EXISTS `toq_db`.`complex_towers` (
-  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  `complex_id` INT UNSIGNED NOT NULL,
-  `tower` VARCHAR(45) NOT NULL,
-  `floors` INT NULL DEFAULT NULL,
-  `total_units` INT NULL DEFAULT NULL,
-  `units_per_floor` INT NULL DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  INDEX `torre_emp_idx` (`complex_id` ASC) VISIBLE,
-  CONSTRAINT `fk_complex_tower`
-    FOREIGN KEY (`complex_id`)
-    REFERENCES `toq_db`.`complex` (`id`)
-    ON DELETE CASCADE
-    ON UPDATE RESTRICT)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb3;
-
-
--- -----------------------------------------------------
--- Table `toq_db`.`complex_zip_codes`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `toq_db`.`complex_zip_codes` ;
-
-CREATE TABLE IF NOT EXISTS `toq_db`.`complex_zip_codes` (
-  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  `complex_id` INT UNSIGNED NOT NULL,
-  `zip_code` VARCHAR(9) NOT NULL,
-  PRIMARY KEY (`id`),
-  INDEX `cep_emp_idx` (`complex_id` ASC) VISIBLE,
-  CONSTRAINT `fk_complex_zip`
-    FOREIGN KEY (`complex_id`)
-    REFERENCES `toq_db`.`complex` (`id`)
-    ON DELETE CASCADE)
-ENGINE = InnoDB
-AUTO_INCREMENT = 566
-DEFAULT CHARACTER SET = utf8mb3;
-
-
--- -----------------------------------------------------
 -- Table `toq_db`.`configuration`
 -- -----------------------------------------------------
 DROP TABLE IF EXISTS `toq_db`.`configuration` ;
@@ -1068,10 +981,12 @@ START TRANSACTION;
 -- Esvaziar as tabelas antes de carregar os novos dados
 -- Use TRUNCATE TABLE para limpar os dados de forma eficiente.
 -- TRUNCATE TABLE base_features;
--- TRUNCATE TABLE complex_zip_codes;
--- TRUNCATE TABLE complex_towers;
--- TRUNCATE TABLE complex_sizes;
--- TRUNCATE TABLE complex;
+-- TRUNCATE TABLE vertical_complexes;
+-- TRUNCATE TABLE vertical_complex_towers;
+-- TRUNCATE TABLE vertical_complex_sizes;
+-- TRUNCATE TABLE horizontal_complexes;
+-- TRUNCATE TABLE horizontal_complex_zip_codes;
+-- TRUNCATE TABLE no_complex_zip_codes;
 -- TRUNCATE TABLE role_permissions;
 -- TRUNCATE TABLE permissions;
 -- TRUNCATE TABLE roles;
@@ -1087,38 +1002,53 @@ ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
 IGNORE 1 LINES;
 
-LOAD DATA INFILE '/var/lib/mysql-files/complex.csv'
-INTO TABLE complex
+LOAD DATA INFILE '/var/lib/mysql-files/vertical_complexes.csv'
+INTO TABLE vertical_complexes
 FIELDS TERMINATED BY ';'
 ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
 IGNORE 1 LINES
 (id, name, zip_code, street, number, neighborhood, city, state, reception_phone, sector, main_registration, type);
 
-LOAD DATA INFILE '/var/lib/mysql-files/complex_sizes.csv'
-INTO TABLE complex_sizes
+LOAD DATA INFILE '/var/lib/mysql-files/vertical_complex_towers.csv'
+INTO TABLE vertical_complex_towers
 FIELDS TERMINATED BY ';'
 ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
 IGNORE 1 LINES
-(id, complex_id, size, description);
+(id, vertical_complex_id, tower, floors, total_units, units_per_floor);
 
-LOAD DATA INFILE '/var/lib/mysql-files/complex_towers.csv'
-INTO TABLE complex_towers
+LOAD DATA INFILE '/var/lib/mysql-files/vertical_complex_sizes.csv'
+INTO TABLE vertical_complex_sizes
 FIELDS TERMINATED BY ';'
 ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
 IGNORE 1 LINES
-(id, complex_id, tower, floors, total_units, units_per_floor);
+(id, vertical_complex_id, size, description);
 
-LOAD DATA INFILE '/var/lib/mysql-files/complex_zip_codes.csv'
-INTO TABLE complex_zip_codes
+LOAD DATA INFILE '/var/lib/mysql-files/horizontal_complexes.csv'
+INTO TABLE horizontal_complexes
 FIELDS TERMINATED BY ';'
 ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
 IGNORE 1 LINES
-(id, complex_id, zip_code);
+(id, name, zip_code, street, number, neighborhood, city, state, reception_phone, sector, main_registration, type);
 
+LOAD DATA INFILE '/var/lib/mysql-files/horizontal_complex_zip_codes.csv'
+INTO TABLE horizontal_complex_zip_codes
+FIELDS TERMINATED BY ';'
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 LINES
+(id, horizontal_complex_id, zip_code);
+
+LOAD DATA INFILE '/var/lib/mysql-files/no_complex_zip_code.csv'
+INTO TABLE no_complex_zip_codes
+FIELDS TERMINATED BY ';'
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 LINES
+(zip_code, neighborhood, city, state, sector, type);
 
 -- Importar roles
 LOAD DATA INFILE '/var/lib/mysql-files/base_permission_roles.csv'
